cb592f55d5ead92537e50ecd4fb8c9554e36f2b8
Claude BUENO <hello@bueno.fr>
2022-10-14 19:25:10 +0200

    Design with tailwincss

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss']
+})
+export class FaceSnapComponent implements OnInit {
+  title!: string;
+  description!: string;
+  createDate!: Date;
+  snaps!: number;
+  imageUrl!: string;
+  buttonText!: string;
+
+  ngOnInit() {
+    this.title = 'Archibald';
+    this.description = 'Mon meilleur ami depuis tout petit !';
+    this.createDate = new Date();
+    this.snaps = 6;
+    this.buttonText = 'Oh Snap!';
+    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+  }
+  onSnap() {
+    if (this.buttonText === 'Oh Snap!') {
+      this.snaps++;
+      this.buttonText = 'Oops, unSnap!';
+    } else {
+      this.snaps--;
+      this.buttonText = 'Oh Snap!';
+    }
+  }
+}
